7278b071cf0beb0b69f4735e8d92648ad338e9d4
Thibault Balleux <balleux02@icloud.com>
2024-05-17 16:43:24 +0200

    Text changes

--- src/main.py
+++ src/main.py
@@ -3,5 +3,5 @@ def greet(name) -> str:
     return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    user_name = input("Enter your name: ")
+    user_name = input("Enter your username: ")
     print(greet(user_name))
